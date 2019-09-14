d367dcd2d3c272bb8bccd7eed098b90197d06d0f
nishantk03 <nishant.bharwal@gmail.com>
2019-09-15 04:06:20 +0530

    #Card layout added for new product

--- VegCart/src/app/admin/product-form/product-form.component.ts
+++ VegCart/src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -12,7 +13,8 @@ export class ProductFormComponent implements OnInit {
   categories$;
 
   constructor(categoryService: CategoryService,
-              private productService: ProductService) {
+              private productService: ProductService,
+              private router:Router) {
     this.categories$ = categoryService.getCategories()
   }
 
@@ -21,6 +23,7 @@ export class ProductFormComponent implements OnInit {
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
 }
