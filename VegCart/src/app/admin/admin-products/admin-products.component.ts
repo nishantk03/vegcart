e6f2bf31bbea797d3310e4ed4c1fcabb828d16e6
nishantk03 <nishant.bharwal@gmail.com>
2019-09-15 05:53:49 +0530

    #Filter on products

--- VegCart/src/app/admin/admin-products/admin-products.component.ts
+++ VegCart/src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$;
+  products: { title: string }[];
+  filteredProduct: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
-   }
+    this.subscription = this.productService.getAll()
+      .subscribe(res =>
+        this.filteredProduct = this.products = res);//Here we will not use take(1) as user can edit in multi window so it will not reflect that is y ngOnDestroy
+  }
 
   ngOnInit() {
+  }
+
+  filter(query: string) {
+    this.filteredProduct = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
